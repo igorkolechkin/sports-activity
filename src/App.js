0ae030663f772291802fc76dab59604464d1cdb1
igorkolechkindev <igorko@wizardsdev.com>
2020-07-14 14:28:30 +0300

    clean custom config and install react form create-react-app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import Test from '@components/test';
-import MyImage from '@images/icon/flying-saucer-img.png';
+import logo from './logo.svg';
+import './App.css';
 
-export default () => {
+function App() {
   return (
-    <div>
-      <Test />
-      <img src={ MyImage } alt=""/>
-      <p>adas</p>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )
+  );
 }
+
+export default App;
